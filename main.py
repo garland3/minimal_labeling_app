0c36cae26fe5ec760dfa98b91ede1f5a2f785367
anthony garland <garland3@gmail.com>
2024-07-05 07:48:01 -0600

    added gif

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import json
@@ -19,8 +19,16 @@ templates = Jinja2Templates(directory="templates")
 # Specify the image directory
 IMG_DIR = Path("my_cool_imgs")  # Replace with your actual image directory path
 
+# Mount the image directory
+app.mount("/images", StaticFiles(directory=IMG_DIR), name="images")
+
 # Get a list of all image files in the directory
 image_files = sorted([str(file.relative_to(IMG_DIR)) for file in IMG_DIR.glob("*") if file.suffix.lower() in ['.jpg', '.jpeg', '.png', '.gif']])
+
+output_dir = Path("output")
+output_dir.mkdir(parents=True, exist_ok=True)
+# mount the output directory
+app.mount("/output", StaticFiles(directory=output_dir), name="output")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
@@ -40,25 +48,59 @@ async def get_image(index: int):
 async def get_image_count():
     return {"count": len(image_files)}
 
-# Mount the image directory
-app.mount("/images", StaticFiles(directory=IMG_DIR), name="images")
 
 
 
 @app.post("/submit_coordinates")
 async def submit_coordinates(request: Request):
     data = await request.json()
+    print("data", data)
     currentImageIndex = int(data['currentImageIndex'])
-    # Append coordinates to list
-    # coordinates.extend(data['coordinates'])
-    
+    # add the idx and fielname to the data dict
+    data['idx'] = currentImageIndex
+    data['filename'] = image_files[currentImageIndex]    
     # Save coordinates to file as JSON
-    filename = f"output/coordinates_{currentImageIndex}.json"
+    filename = output_dir/f"coordinates_{currentImageIndex}.json"
     with open(filename, 'w') as f:
-        json.dump(coordinates, f, indent=2)
-    
+        json.dump(data, f, indent=2)
     return {"message": "Coordinates received and stored"}
 
+# show all the output files togehter
+# combine all into one json file. 
+# make a download button that downloads the json file
+file_path_combined_json = output_dir/"combined.json"
+def combine_files():
+    # json_files = sorted([str(file.relative_to(output_dir)) for file in output_dir.glob("coordinates_*") if file.suffix.lower() == '.json'])
+    json_files = list(output_dir.glob("coordinates_*"))
+    combined_dict =[]
+    for j in json_files:
+        with open(j) as f:
+            data = json.load(f)
+            combined_dict.append(data)
+    # save the combined dict to a file
+    # with open(output_dir/"combined.json", 'w') as f:
+    with open(file_path_combined_json, 'w') as f:
+        json.dump(combined_dict, f, indent=2)
+    return combined_dict
+
+@app.get("/download")
+async def download():
+    # read the json files
+    combine_files()
+    return FileResponse(file_path_combined_json, media_type="application/json", filename=f"combined.json")
+
+# view the combined files, but wrapped in a simple html page
+# at /download_view
+@app.get("/download_view")
+async def download_view():
+    myjson = combine_files()
+    return HTMLResponse( f""" 
+    <html>
+        <body>
+            {json.dumps(myjson, indent=2)}
+        </body>
+    </html>
+    """)
 
 
 @app.get("/get_coordinates")
--- readme.md
+++ readme.md
@@ -1,5 +1,10 @@
 # minimal quick label app
 
+Put your images in 
+`my_cool_imgs` folder and run the app.
+
+will give you json output of the bounding box.
+
 
 ```bash
 conda create -n quick_label_app python=3.11 -y
@@ -8,3 +13,9 @@ pip install fastapi uvicorn
 
 uvicorn main:app --reload
 ```
+
+## Screen shot video.
+
+[![BBox GIF](readme_imgs/bbox.gif)](readme_imgs/bbox.gif)
+
+
